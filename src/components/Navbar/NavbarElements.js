c9ec5fece427658714642216edce88e94ccb4a37
Kishan Jasani <kishanjasani007@yahoo.in>
2020-10-19 20:52:10 +0530

    Add Hero Section to template

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -5,6 +5,7 @@ import { Link as LinkScroll } from 'react-scroll';
 export const Nav = styled.nav`
 	background: #000;
 	height: 80px;
+	margin-top: -80px;
 	font-size: 1rem;
 	position: sticky;
 	top: 0;
